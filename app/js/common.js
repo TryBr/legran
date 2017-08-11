ce8bfd2ab3f4b9e3096b123cf2070cd8f77adada
Trybr <Avulsed.jb@gmail.com>
2017-08-11 08:20:04 +0500

    pre final push

--- app/js/common.js
+++ app/js/common.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 			slidesPerView: 6,
 			paginationClickable: true,
 			spaceBetween: 0,
-			loop: true,
+			loop: false,
 			breakpoints: {
 				// when window width is <= 320px
 				320: {
@@ -37,6 +37,73 @@ $(document).ready(function() {
 
 	$('.series-nav-next').click(function() {
 			swiperSeriesSlider.slideNext();
+	});
+
+
+
+	$('#gallery').click(function() {
+			$('.series-gallery').show();
+
+			/*---------------
+					#SERIES GALLERY
+			---------------*/
+			var swiperSeriesGallery = new Swiper('.swiper-series-gallery', {
+					pagination: '.swiper-pagination',
+					paginationType: 'fraction'
+			});
+
+			$('.gallery-nav-prev').click(function() {
+					swiperSeriesGallery.slidePrev();
+			});
+
+			$('.gallery-nav-next').click(function() {
+					swiperSeriesGallery.slideNext();
+			});
+	});
+
+	$('#gallery-close').click(function() {
+			$('.series-gallery').hide();
+	});
+
+
+
+	/*---------------
+			#SERIES SLIDER ADDITIONAL
+	---------------*/
+	var swiperSeriesSliderAd = new Swiper('.swiper-series-additional', {
+			slidesPerView: 6,
+			paginationClickable: true,
+			spaceBetween: 0,
+			loop: false,
+			breakpoints: {
+				// when window width is <= 320px
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 480px
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 640px
+				767: {
+					slidesPerView: 2,
+					spaceBetween: 0
+				},
+				1199: {
+					slidesPerView: 4,
+					spaceBetween: 0
+				}
+			}
+	});
+
+	$('.series-nav-ad-prev').click(function() {
+			swiperSeriesSliderAd.slidePrev();
+	});
+
+	$('.series-nav-ad-next').click(function() {
+			swiperSeriesSliderAd.slideNext();
 	});
 
 
@@ -71,12 +138,90 @@ $(document).ready(function() {
 			}
 	});
 
-	$('.products-nav-prev').click(function() {
+	$('.products-nav-main-prev').click(function() {
 			swiperProducts.slidePrev();
 	});
 
-	$('.products-nav-next').click(function() {
+	$('.products-nav-main-next').click(function() {
 			swiperProducts.slideNext();
+	});
+
+	/*---------------
+			#PRODUCTS SLIDER INNER
+	---------------*/
+	var swiperProductsInner = new Swiper('.swiper-products-inner', {
+			slidesPerView: 2,
+			paginationClickable: true,
+			loop: true,
+			spaceBetween: 0,
+			breakpoints: {
+				// when window width is <= 320px
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 480px
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 640px
+				767: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				1199: {
+					slidesPerView: 2,
+					spaceBetween: 30
+				}
+			}
+	});
+
+	$('.products-nav-in-prev').click(function() {
+			swiperProductsInner.slidePrev();
+	});
+
+	$('.products-nav-in-next').click(function() {
+			swiperProductsInner.slideNext();
+	});
+
+	/*---------------
+			#PRODUCTS SLIDER ADDITIONAL
+	---------------*/
+	var swiperProductsAd = new Swiper('.swiper-products-additional', {
+			slidesPerView: 2,
+			paginationClickable: true,
+			loop: true,
+			spaceBetween: 0,
+			breakpoints: {
+				// when window width is <= 320px
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 480px
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 640px
+				767: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				1199: {
+					slidesPerView: 2,
+					spaceBetween: 30
+				}
+			}
+	});
+
+	$('.products-nav-ad-prev').click(function() {
+			swiperProductsAd.slidePrev();
+	});
+
+	$('.products-nav-ad-next').click(function() {
+			swiperProductsAd.slideNext();
 	});
 
 
@@ -117,6 +262,166 @@ $(document).ready(function() {
 
 	$('.references-nav-next').click(function() {
 			swiperReferences.slideNext();
+	});
+
+
+	/*---------------
+			#SERIES COLOR SLIDER
+	---------------*/
+	var swiperSeriesColorMain = new Swiper('.swiper-colors-main', {
+			slidesPerView: 3,
+			paginationClickable: true,
+			spaceBetween: 10,
+			breakpoints: {
+				// when window width is <= 320px
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 480px
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 640px
+				767: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				1199: {
+					slidesPerView: 2,
+					spaceBetween: 30
+				}
+			}
+	});
+
+
+	$('.series-colors-nav-prev').click(function() {
+			swiperSeriesColorMain.slidePrev();
+	});
+
+	$('.series-colors-nav-next').click(function() {
+			swiperSeriesColorMain.slideNext();
+	});
+
+	/*---------------
+			#SERIES COLOR SLIDER 2
+	---------------*/
+	var swiperSeriesColorAdditional = new Swiper('.swiper-colors-additional', {
+			slidesPerView: 3,
+			paginationClickable: true,
+			spaceBetween: 10,
+			breakpoints: {
+				// when window width is <= 320px
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 480px
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 640px
+				767: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				1199: {
+					slidesPerView: 2,
+					spaceBetween: 30
+				}
+			}
+	});
+
+
+	$('.series-colors-nav-prev-2').click(function() {
+			swiperSeriesColorAdditional.slidePrev();
+	});
+
+	$('.series-colors-nav-next-2').click(function() {
+			swiperSeriesColorAdditional.slideNext();
+	});
+
+
+	/*-------------------*/
+
+	/*---------------
+			#SERIES COLOR SLIDER 2
+	---------------*/
+	var swiperSeriesColorMain2 = new Swiper('.swiper-colors-main-2', {
+			slidesPerView: 3,
+			paginationClickable: true,
+			spaceBetween: 10,
+			breakpoints: {
+				// when window width is <= 320px
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 480px
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 640px
+				767: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				1199: {
+					slidesPerView: 2,
+					spaceBetween: 30
+				}
+			}
+	});
+
+
+	$('.series-colors-nav-prev-11').click(function() {
+			swiperSeriesColorMain2.slidePrev();
+	});
+
+	$('.series-colors-nav-next-11').click(function() {
+			swiperSeriesColorMain2.slideNext();
+	});
+
+	/*---------------
+			#SERIES COLOR SLIDER 2
+	---------------*/
+	var swiperSeriesColorAdditional2 = new Swiper('.swiper-colors-additional-2', {
+			slidesPerView: 3,
+			paginationClickable: true,
+			spaceBetween: 10,
+			breakpoints: {
+				// when window width is <= 320px
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 480px
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				// when window width is <= 640px
+				767: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				1199: {
+					slidesPerView: 2,
+					spaceBetween: 30
+				}
+			}
+	});
+
+
+	$('.series-colors-nav-prev-22').click(function() {
+			swiperSeriesColorAdditional2.slidePrev();
+	});
+
+	$('.series-colors-nav-next-22').click(function() {
+			swiperSeriesColorAdditional2.slideNext();
 	});
 
 
@@ -185,8 +490,10 @@ $(document).ready(function() {
 		}
 	 });
 
-	
-
-
+	 $('.series-information__link').click(function() {
+		 $('html, body').animate({
+				 scrollTop: $( $.attr(this, 'href') ).offset().top
+		 }, 500);
+ 	 });
 
 });
